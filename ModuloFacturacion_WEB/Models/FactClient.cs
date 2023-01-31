73ee2bb51378e7faf54412f755ada039d61ee0a5
NAYELI ALEXANDRA CUASQUER NARVAEZ <nacuasquern@utn.edu.ec>
2023-01-31 08:14:02 -0500

    Validaciones

--- ModuloFacturacion_WEB/Models/FactClient.cs
+++ ModuloFacturacion_WEB/Models/FactClient.cs
@@ -1,19 +1,28 @@
-﻿using Microsoft.Build.Framework;
+﻿using System.ComponentModel.DataAnnotations;
+
 
 namespace ModuloFacturacion_WEB.Models
 {
     public class FactClient
     {
+        [Required(ErrorMessage = "Ingrese la Cédula")]
+        [StringLength(maximumLength: 10, MinimumLength = 10, ErrorMessage = "Cédula Inválida")]
         public string CliIdentification { get; set; } = null!;
-        [Required]
+
+        [Required(ErrorMessage = "Debe ingresar el Nombre del Cliente")]
+        [StringLength(maximumLength: 50, MinimumLength = 2, ErrorMessage = "El Nombre es muy corto")]
         public string? CliName { get; set; }
 
         public DateTime? CliBirthday { get; set; }
 
+        [Required(ErrorMessage = "Ingrese una Dirección")]
         public string? CliAddres { get; set; }
 
+        [StringLength(maximumLength: 10, MinimumLength = 10, ErrorMessage = "El número es inválido")]
+        [RegularExpression("(^[0-9]+$)", ErrorMessage = "Número Invalido")]
         public string? CliPhone { get; set; }
 
+        [EmailAddress(ErrorMessage = "El correo es inválido")]
         public string? CliMail { get; set; }
 
         public bool? CliStatus { get; set; }
